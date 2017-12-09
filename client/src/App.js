1b346ca96d2584171af54b1f7fa28c348aaef77c
Sihan Zhao <zhsh0616@gmail.com>
2017-12-08 20:22:15 -0600

    Update Dev Env; no bootstrap-theme.css anymore

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Router, Route, NavLink,Switch } from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap';
+import History from './History';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	<h1>hello world</h1>
       </div>
     );
   }
