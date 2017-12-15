792bc8fb1d0620710e41a99f4d2d3de7013e93d9
Sihan Zhao <zhsh0616@gmail.com>
2017-12-14 21:15:43 -0600

    change to bootstrap@3

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,37 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Router, Route, NavLink,Switch } from 'react-router-dom';
 import {LinkContainer} from 'react-router-bootstrap';
 import History from './History';
+import Footer from './Footer';
+import FontAwesome from 'react-fontawesome';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-	<h1>hello world</h1>
-      </div>
+	<div className="site">
+	  <Router history = {History}>
+	    <div className="site-content">
+	      <Navbar fluid >
+		<Navbar.Header>
+		  <Navbar.Brand>
+		    <a href="/">Map</a>
+		  </Navbar.Brand>
+		</Navbar.Header>
+                  <Nav >
+                    <NavItem>
+		      <p>News</p>
+                    </NavItem>
+		    <LinkContainer to="/faq">
+                    <NavItem>
+                      <p>FAQ</p>
+                    </NavItem>
+                  </LinkContainer>
+		  </Nav>
+	      </Navbar>
+	    </div>
+	    </Router>
+	  <Footer/>
+	</div>
     );
   }
 }
---
+++ client/src/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+  
+  return (
+	  <div className="footer">
+	  <a href="https://github.com/LarryZhao0616/chicago-crime-map"><i href="https://github.com/LarryZhao0616/chicago-crime-map" className="fa fa-github-square fa-2x fa-cog" aria-hidden="true"></i></a>
+	  <a href="http://www.facebook.com"><i className="fa fa-facebook-square fa-2x fa-cog" href="/" aria-hidden="true"></i></a>
+	  <hr />
+	  <p>Copyright &copy;2017 All Rights Reserved</p>
+	  </div>
+  );
+};
+//<div className="container" id="social-network-links">
+  //      {links.map(linkItem => {
+//          return (
+          //  <a href={linkItem.link} key={linkItem.link}>
+        //      <img src={linkItem.image} alt="social network link" />
+      //      </a>
+    //      );
+  //      })}
+//      </div>
+export default Footer;
